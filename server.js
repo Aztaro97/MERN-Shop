53c91972b0aeffd62694e28ae88a35ae0821b459
aztaro97 <aztaro97@gmail.com>
2021-09-06 18:31:46 +0400

    Update

--- server.js
+++ server.js
@@ -61,12 +61,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+app.set("port", PORT);
 
 // Creating uploads folder if not already present
 // In "uploads" folder we will temporarily upload
