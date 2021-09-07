3fa8d8317d53525df7efa7a9d79284995fbe1a58
aztaro97 <aztaro97@gmail.com>
2021-09-07 18:55:25 +0400

    update

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./flux/store";
 import "./translations/i18next";
-// import Loader from "./components/Loader.jsx"
+import Loader from "./components/Loader"
 import 'flag-icon-css/css/flag-icon.min.css'
 import "react-toastify/dist/ReactToastify.css";
 
@@ -12,7 +12,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 ReactDOM.render(
-  <Suspense >
+  <Suspense fallback={Loader}>
     <Provider store={store}>
       <App />
     </Provider>
